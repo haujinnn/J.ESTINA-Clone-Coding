68587b0b2c59ae01f3872ac7b00dede211ae5158
haujin <uj0110@g.skku.edu>
2021-07-22 00:27:49 +0900

    html finish

--- main.js
+++ main.js
@@ -1,20 +1,7 @@
-{/* .btn2를 찾고 그것을 클릭하면 아래 이벤트가 일어나게 하라 */}
-document.querySelector('.btn1').addEventListener('click', function(){
-    document.querySelector('.container').style.transform = 'translate(0vw)';
-});
 
-document.querySelector('.btn2').addEventListener('click', function(){
-    document.querySelector('.container').style.transform = 'translate(-100vw)';
-});
-
-document.querySelector('.btn3').addEventListener('click', function(){
-    document.querySelector('.container').style.transform = 'translate(-200vw)';
-});
-
-document.querySelector('.btn4').addEventListener('click', function(){
-    document.querySelector('.container').style.transform = 'translate(-300vw)';
-});
-
-document.querySelector('.btn5').addEventListener('click', function(){
-    document.querySelector('.container').style.transform = 'translate(-400vw)';
-});
+$('.visual').slick({
+    dots:true,
+    autoplay:true,
+    pauseOnHover : true,
+    dotsClass : "slick-dots" //점의 css class 지정
+}); //JQuery 라이브러리 필요
